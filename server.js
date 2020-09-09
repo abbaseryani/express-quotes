ef854d9b2e62038379bfd95c384e16441cad1985
Abbas Al-Eryani <Abbas.Al-Eryani@evotec.com>
2020-09-09 15:32:53 +0200

    Add simple get and post requests

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("Hello Node");
+const express = require('express')
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient
+const app = express();
+
+MongoClient.connect('', (err, client) => {
+
+});
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/templates/index.html");
+});
+
+app.post('/quotes', (req, res) => {
+    console.log(req.body);
+});
+app.listen(3000, () => {});

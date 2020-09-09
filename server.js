a27254884deba40a1dfcf6152c5f390c8248475c
Abbas Al-Eryani <Abbas.Al-Eryani@evotec.com>
2020-09-09 16:10:57 +0200

    Add connection to Mongodb

--- server.js
+++ server.js
@@ -2,17 +2,24 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient
 const app = express();
+const connectionString = 'mongodb+srv://admin:admin@quotescluster.inqzv.mongodb.net/<dbname>?retryWrites=true&w=majority'
 
-MongoClient.connect('', (err, client) => {
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+    .then(client => {
+        const db = client.db('quotes-db')
+        const quotesCollection = db.collection('quotes')
+        app.use(bodyParser.urlencoded({ extended: true }));
 
-});
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/templates/index.html");
-});
-
-app.post('/quotes', (req, res) => {
-    console.log(req.body);
-});
-app.listen(3000, () => {});
+        app.get('/', (req, res) => {
+            res.sendFile(__dirname + "/templates/index.html");
+        });
+        app.post('/quotes', (req, res) => {
+            quotesCollection.insertOne(req.body)
+                .then(result => {
+                    res.redirect('/');
+                })
+                .catch(error => console.error(error))
+        });
+        app.listen(3000, () => {});
+    })
+    .catch(error => console.error(error))
